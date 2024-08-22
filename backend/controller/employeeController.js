df8ecd828c2356ff9a12166be30e95efc09f9e4b
Andrea-Graf <andrea.graf@heig-vd.ch>
2024-08-22 09:19:06 +0200

    Create employee controller

---
+++ backend/controller/employeeController.js
@@ -0,0 +1,10 @@
+// Get tous les employes - /employes
+export async function getEmployees(req, res) {}
+// Get un employe par id - /employes/:id
+export async function getEmployeeById(req, res) {}
+// Créer un employe - /employes
+export async function createEmployee(req, res) {}
+// Mettre à jour un employe - /employes/:id
+export async function updateEmployee(req, res) {}
+// Supprimer un employe - /employes/:id
+export async function deleteEmployee(req, res) {}
