68db9215aa4922edf3ed71156784e339d545d836
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-22 09:39:21 +0200

    add test for employe on diffrent decheterie

--- backend/tests/employe.test.js
+++ backend/tests/employe.test.js
@@ -359,4 +359,134 @@ describe("Employe CRUD", () => {
   });
 });
 
-// TODO: test si la déchèterie principale est la même que celle de l'employé
+describe("Employe CRUD with different decheterie", () => {
+  test("Responsable", async () => {
+    const list = await request(app).post("/api/login").send(Responsable);
+    const cookie = list.headers["set-cookie"];
+
+    const list2 = await request(app).post("/api/login").send(Responsable2);
+    const cookie2 = list2.headers["set-cookie"];
+
+    // create a employe with in a different primary decheterie
+    const employe = await request(app)
+      .post("/api/employes")
+      .set("Cookie", cookie)
+      .send({
+        idlogin: "tdoumas",
+        mdplogin: "123",
+        nom: "Doumas",
+        prenom: "Tristan",
+        datenaissance: "1990-01-01",
+        datedebutcontrat: "2022-01-01",
+        fk_fonction: "Chauffeur",
+        numtelephone: "1234567890",
+        typepermis: "C",
+        fk_adresse: 1,
+        fk_decheterie: 6,
+      });
+    expect(employe.statusCode).toEqual(500);
+    expect(employe.body).toEqual({
+      error: "Error adding employe",
+    });
+
+    // create a employe with in the same primary decheterie
+    const employe2 = await request(app)
+      .post("/api/employes")
+      .set("Cookie", cookie2)
+      .send({
+        idlogin: "tdoumas",
+        mdplogin: "123",
+        nom: "Doumas",
+        prenom: "Tristan",
+        datenaissance: "1990-01-01",
+        datedebutcontrat: "2022-01-01",
+        fk_fonction: "Chauffeur",
+        numtelephone: "1234567890",
+        typepermis: "C",
+        fk_adresse: 1,
+        fk_decheterie: 6,
+      });
+    expect(employe2.statusCode).toEqual(201);
+    expect(employe2.body).toEqual({
+      message: "Employe added successfully",
+    });
+
+    // get one
+    const employeGet = await request(app)
+      .get("/api/employes/6")
+      .set("Cookie", cookie);
+    expect(employeGet.statusCode).toEqual(500);
+    expect(employeGet.body).toEqual({
+      error: "Error getting employe",
+    });
+
+    // get all
+    const employeGetAll = await request(app)
+      .get("/api/employes")
+      .set("Cookie", cookie);
+    expect(employeGetAll.statusCode).toEqual(200);
+    expect(employeGetAll.body).toEqual({
+      employes: [],
+    });
+
+    // get all
+    const employeGetAll2 = await request(app)
+      .get("/api/employes")
+      .set("Cookie", cookie2);
+    expect(employeGetAll2.statusCode).toEqual(200);
+    expect(employeGetAll2.body).toEqual({
+      employes: [
+        {
+          id_decheterie: 1,
+          nom_decheterie: "Decheterie Yverdon",
+          id_employe: "tdoumas",
+          nom_employe: "Doumas",
+          prenom_employe: "Tristan",
+          fonction_employe: "Chauffeur",
+          date_naissance: "1990-01-01",
+          date_debut_contrat: "2022-01-01",
+          numero_telephone: "1234567890",
+          type_permis: "C",
+        },
+      ],
+    });
+
+    // update
+    const employeUpdate = await request(app)
+      .put("/api/employes/6")
+      .set("Cookie", cookie)
+      .send({
+        idlogin: "tdoumas",
+        mdplogin: "123",
+        nom: "Doumas",
+        prenom: "Tristan",
+        datenaissance: "1990-01-01",
+        datedebutcontrat: "2022-01-01",
+        fk_fonction: "Chauffeur",
+        numtelephone: "1234567890",
+        typepermis: "C",
+        fk_adresse: 1,
+        fk_decheterie: 6,
+      });
+    expect(employeUpdate.statusCode).toEqual(500);
+    expect(employeUpdate.body).toEqual({
+      message: "Error updating employe",
+    });
+
+    // delete
+    const employeDelete = await request(app)
+      .delete("/api/employes/6")
+      .set("Cookie", cookie);
+    expect(employeDelete.statusCode).toEqual(500);
+    expect(employeDelete.body).toEqual({
+      message: "Error deleting employe",
+    });
+    const employeDelete2 = await request(app)
+      .delete("/api/employes/6")
+      .set("Cookie", cookie2);
+    expect(employeDelete2.statusCode).toEqual(200);
+    expect(employeDelete2.body).toEqual({
+      message: "Employe deleted successfully",
+    });
+  });
+});
