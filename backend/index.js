21d79a18d604549fc1be399d521b9e94b29cad3e
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-23 15:08:00 +0200

    test db ok in github test

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 import app from "./server.js";
 import { initializeDB } from "./database/orm.js";
+import { models } from "./database/orm.js";
 
 console.log("Starting server...");
 await initializeDB();
 app.listen(80, () => {
   console.log(`Listening on port 80`);
 });
+
+console.log("Test db ok");
+console.log(models.Employe.findAll());
