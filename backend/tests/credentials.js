d3469385fa645a35683ec1b1298f712d3081af3c
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-23 14:39:44 +0200

    remove error test

--- backend/tests/credentials.js
+++ backend/tests/credentials.js
@@ -7,3 +7,14 @@ const Responsable2 = { username: "jdurand", password: "123" }; // déchèterie 5
 const Secretaire2 = { username: "jdoe3", password: "123" }; // déchèterie 5
 const Employe2 = { username: "rlandry", password: "123" }; // déchèterie 6
 const Chauffeur2 = { username: "lchevalier", password: "123" }; // déchèterie 5
+
+export {
+  Responsable,
+  Secretaire,
+  Employe,
+  Chauffeur,
+  Responsable2,
+  Secretaire2,
+  Employe2,
+  Chauffeur2,
+};
