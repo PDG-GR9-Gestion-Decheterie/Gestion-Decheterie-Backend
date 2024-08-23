96b1d79749a8cbc5002ec3790aecc354ed3115fd
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-23 16:49:22 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,7 @@ passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
       const user = await models.Employe.findByPk(username);
+      console.log("User found:", user);
       if (user === null) {
         // User not found, perform a dummy bcrypt check to mitigate timing attacks
         await bcrypt.compare(password, "$2b$10$dummyhashdummyhashdummyhashdum");
