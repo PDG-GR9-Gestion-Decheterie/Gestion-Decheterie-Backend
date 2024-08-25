1b2d29ebe423586faf2718bd53d294288e4cc5bc
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-25 14:59:32 +0200

    change init db test

--- backend/tests/jest.setup.js
+++ backend/tests/jest.setup.js
@@ -1,1 +1,6 @@
-import "../index.js";
+import { initializeDB } from "../database/orm.js";
+
+beforeAll(async () => {
+  // Initialize DB connection before running any tests
+  await initializeDB();
+});
