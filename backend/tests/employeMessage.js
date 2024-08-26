65a28f1d7349c61ccec6295697ff2ef21afdb558
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-26 10:10:07 +0200

    modify test

---
+++ backend/tests/employeMessage.js
@@ -0,0 +1,117 @@
+const tdoumasRequest = {
+  idlogin: "tdoumas",
+  mdplogin: "123",
+  nom: "Doumas",
+  prenom: "Tristan",
+  datenaissance: "1990-01-01",
+  datedebutcontrat: "2022-01-01",
+  fk_fonction: "Chauffeur",
+  numtelephone: "1234567890",
+  typepermis: "C",
+  fk_adresse: 1,
+  fk_decheterie: 1,
+};
+
+const tdoumasUpdateRequest = {
+  idlogin: "tdoumas",
+  mdplogin: "password",
+  nom: "TriTri",
+  prenom: "TriTri",
+  datenaissance: "1990-01-01",
+  datedebutcontrat: "2022-01-01",
+  fk_fonction: "Chauffeur",
+  numtelephone: "1234567890",
+  typepermis: "C",
+  fk_adresse: 1,
+  fk_decheterie: 1,
+};
+
+const tdoumasGetOneResponse = {
+  employes: {
+    idlogin: "tdoumas",
+    nom: "Doumas",
+    prenom: "Tristan",
+    datenaissance: "1990-01-01",
+    datedebutcontrat: "2022-01-01",
+    numtelephone: "1234567890",
+    typepermis: "C",
+    fk_adresse: 1,
+    fk_decheterie: 1,
+    fk_fonction: "Chauffeur",
+  },
+};
+
+const tdoumasGetAllResponse = {
+  // todo
+  employes: {
+    idlogin: "tdoumas",
+    nom: "Doumas",
+    prenom: "Tristan",
+    datenaissance: "1990-01-01",
+    datedebutcontrat: "2022-01-01",
+    numtelephone: "1234567890",
+    typepermis: "C",
+    fk_adresse: 1,
+    fk_decheterie: 1,
+    fk_fonction: "Chauffeur",
+  },
+};
+
+const jdoeRequest = {
+  idlogin: "jdoe",
+  mdplogin: "123",
+  nom: "Doumas",
+  prenom: "Tristan",
+  datenaissance: "1990-01-01",
+  datedebutcontrat: "2022-01-01",
+  fk_fonction: "Chauffeur",
+  numtelephone: "1234567890",
+  typepermis: "C",
+  fk_adresse: 1,
+  fk_decheterie: 2,
+};
+
+const jdoeUpdateRequest = {
+  idlogin: "jdoe",
+  mdplogin: "password",
+  nom: "TriTri",
+  prenom: "TriTri",
+  datenaissance: "1990-01-01",
+  datedebutcontrat: "2022-01-01",
+  fk_fonction: "Chauffeur",
+  numtelephone: "1234567890",
+  typepermis: "C",
+  fk_adresse: 1,
+  fk_decheterie: 2,
+};
+
+const jdoeGetOneResponse = {
+  employes: {
+    idlogin: "jdoe",
+    nom: "Doumas",
+    prenom: "Tristan",
+    datenaissance: "1990-01-01",
+    datedebutcontrat: "2022-01-01",
+    numtelephone: "1234567890",
+    typepermis: "C",
+    fk_adresse: 1,
+    fk_decheterie: 2,
+    fk_fonction: "Chauffeur",
+  },
+};
+
+const jdoeGetAllResponse = {
+  // todo
+  employes: {
+    idlogin: "jdoe",
+    nom: "Doumas",
+    prenom: "Tristan",
+    datenaissance: "1990-01-01",
+    datedebutcontrat: "2022-01-01",
+    numtelephone: "1234567890",
+    typepermis: "C",
+    fk_adresse: 1,
+    fk_decheterie: 2,
+    fk_fonction: "Chauffeur",
+  },
+};
