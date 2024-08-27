7b7eab3de1421f2541f58e5028a913289be62685
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-27 11:15:29 +0200

    add test decheterie

---
+++ backend/tests/decheterieMessage.js
@@ -0,0 +1,124 @@
+const DecheterieOK = {
+  add: "Decheterie added successfully",
+  update: "Decheterie updated successfully",
+  delete: "Decheterie deleted successfully",
+};
+const DecheterieKO = {
+  add: "Error adding decheterie",
+  update: "Error updating decheterie",
+  delete: "Error deleting decheterie",
+  get: "Error getting decheterie",
+};
+
+const decheterie1CreateRequest = {
+  id: 10,
+  nom: "Decheterie Test",
+  fk_adresse: 1,
+};
+
+const decheterie1UpdateRequest = {
+  id: 10,
+  nom: "Decheterie Tesssssssst",
+  fk_adresse: 1,
+};
+
+const decheterie1GetOneResponse = {
+  DecheterieData: {
+    id: 10,
+    nom: "Decheterie Test",
+    fk_adresse: 1,
+  },
+};
+
+const decheterie1GetAllResponse = {
+  decheteriesData: [
+    {
+      id: 1,
+      nom: "Decheterie Yverdon",
+      adresse_rue: "Rue 1",
+      adresse_numero: "1",
+      adresse_npa: "1400",
+      adresse_nomville: "Yverdon-les-Bains",
+      adresse_pays: "Suisse",
+    },
+    {
+      id: 2,
+      nom: "Decheterie Berne",
+      adresse_rue: "Rue 2",
+      adresse_numero: "2",
+      adresse_npa: "3000",
+      adresse_nomville: "Berne",
+      adresse_pays: "Suisse",
+    },
+    {
+      id: 10,
+      nom: "Decheterie Create",
+      adresse_rue: "Rue 1",
+      adresse_numero: "1",
+      adresse_npa: "1400",
+      adresse_nomville: "Yverdon-les-Bains",
+      adresse_pays: "Suisse",
+    },
+  ],
+};
+
+const decheterie2CreateRequest = {
+  id: 10,
+  nom: "Decheterie Test",
+  fk_adresse: 1,
+};
+
+const decheterie2UpdateRequest = {
+  id: 10,
+  nom: "Decheterie Tesssssssst",
+  fk_adresse: 1,
+};
+
+const decheterie2GetOneResponse = {
+  DecheterieData: {
+    id: 10,
+    nom: "Decheterie Test",
+    fk_adresse: 1,
+  },
+};
+
+const decheterie2GetAllResponse = {
+  decheteriesData: [
+    {
+      id: 1,
+      nom: "Decheterie Yverdon",
+      adresse_rue: "Rue 1",
+      adresse_numero: "1",
+      adresse_npa: "1400",
+      adresse_nomville: "Yverdon-les-Bains",
+      adresse_pays: "Suisse",
+    },
+    {
+      id: 2,
+      nom: "Decheterie Berne",
+      adresse_rue: "Rue 2",
+      adresse_numero: "2",
+      adresse_npa: "3000",
+      adresse_nomville: "Berne",
+      adresse_pays: "Suisse",
+    },
+    {
+      id: 10,
+      nom: "Decheterie Create",
+      adresse_rue: "Rue 1",
+      adresse_numero: "1",
+      adresse_npa: "1400",
+      adresse_nomville: "Yverdon-les-Bains",
+      adresse_pays: "Suisse",
+    },
+  ],
+};
+
+export {
+  DecheterieOK,
+  DecheterieKO,
+  decheterie1CreateRequest,
+  decheterie1UpdateRequest,
+  decheterie1GetOneResponse,
+  decheterie1GetAllResponse,
+};
