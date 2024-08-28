70cd29b18e7d776ecc81becfec35b66d451dc8d0
Andrea Graf <andrea.graf@heig-vd.ch>
2024-08-28 08:39:12 +0200

    Maj -> min

--- backend/controller/dropdownController.js
+++ backend/controller/dropdownController.js
@@ -3,14 +3,14 @@ import { models } from "../database/orm.js";
 // Get tous les Fonctions - /fonctions
 export async function getFonctions(req, res) {
   try {
-    let FonctionsData = null;
+    let fonctionsData = null;
 
-    FonctionsData = await models.Fonction.findAll();
-    if (FonctionsData === null) {
+    fonctionsData = await models.Fonction.findAll();
+    if (fonctionsData === null) {
       throw new Error();
     }
 
-    res.status(200).json({ FonctionsData });
+    res.status(200).json({ fonctionsData });
   } catch (err) {
     console.error("Error fetching fonctions:", err);
     res.status(404).json({ error: "Error" });
@@ -19,14 +19,14 @@ export async function getFonctions(req, res) {
 // Get tous les Status - /status
 export async function getStatus(req, res) {
   try {
-    let StatusData = null;
+    let statusData = null;
 
-    StatusData = await models.Status.findAll();
-    if (StatusData === null) {
+    statusData = await models.Status.findAll();
+    if (statusData === null) {
       throw new Error();
     }
 
-    res.status(200).json({ StatusData });
+    res.status(200).json({ statusData });
   } catch (err) {
     console.error("Error fetching status:", err);
     res.status(404).json({ error: "Error" });
@@ -35,14 +35,14 @@ export async function getStatus(req, res) {
 // Get tous les Dechets - /dechets
 export async function getDechets(req, res) {
   try {
-    let DechetsData = null;
+    let dechetsData = null;
 
-    DechetsData = await models.Dechet.findAll();
-    if (DechetsData === null) {
+    dechetsData = await models.Dechet.findAll();
+    if (dechetsData === null) {
       throw new Error();
     }
 
-    res.status(200).json({ DechetsData });
+    res.status(200).json({ dechetsData });
   } catch (err) {
     console.error("Error fetching dechets:", err);
     res.status(404).json({ error: "Error" });
