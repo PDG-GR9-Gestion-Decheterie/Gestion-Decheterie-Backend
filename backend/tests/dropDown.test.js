20fea94de3dca489b890418ee3ca216bd67debb8
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-28 08:48:23 +0200

    add test dropdown

---
+++ backend/tests/dropDown.test.js
@@ -0,0 +1,173 @@
+import { describe, test, expect } from "@jest/globals";
+import app from "../server.js";
+import request from "supertest";
+
+import {
+  Responsable,
+  Secretaire,
+  Employe,
+  Chauffeur,
+  Responsable2,
+  Secretaire2,
+  Employe2,
+  Chauffeur2,
+} from "./credentials.js";
+
+import { Forbidden, Unauthorized } from "./message.js";
+
+import {
+  statusResponse,
+  dechetResponse,
+  fonctionResponse,
+} from "./dropDownMessage.js";
+
+describe("DropDown not logged in", () => {
+  test("Status", async () => {
+    // Get all
+    const status = await request(app).get("/api/status");
+    expect(status.statusCode).toEqual(401);
+    expect(status.body).toEqual({
+      error: Unauthorized.error,
+    });
+  });
+
+  test("Dechets", async () => {
+    // Get all
+    const dechets = await request(app).get("/api/dechets");
+    expect(dechets.statusCode).toEqual(401);
+    expect(dechets.body).toEqual({
+      error: Unauthorized.error,
+    });
+  });
+
+  test("Fonctions", async () => {
+    // Get all
+    const fonctions = await request(app).get("/api/fonctions");
+    expect(fonctions.statusCode).toEqual(401);
+    expect(fonctions.body).toEqual({
+      error: Unauthorized.error,
+    });
+  });
+});
+
+describe("DropDown logged in", () => {
+  test("Responsable", async () => {
+    const list = await request(app).post("/api/login").send(Responsable);
+    const cookie = list.headers["set-cookie"];
+
+    // Get all
+    const status = await request(app).get("/api/status").set("Cookie", cookie);
+    expect(status.statusCode).toEqual(200);
+    expect(status.body).toEqual({
+      message: statusResponse,
+    });
+
+    // Get all
+    const dechets = await request(app)
+      .get("/api/dechets")
+      .set("Cookie", cookie);
+    expect(dechets.statusCode).toEqual(200);
+    expect(dechets.body).toEqual({
+      message: dechetResponse,
+    });
+
+    // Get all
+    const fonctions = await request(app)
+      .get("/api/fonction")
+      .set("Cookie", cookie);
+    expect(fonctions.statusCode).toEqual(200);
+    expect(fonctions.body).toEqual({
+      message: fonctionResponse,
+    });
+  });
+
+  test("Secretaire", async () => {
+    const list = await request(app).post("/api/login").send(Secretaire);
+    const cookie = list.headers["set-cookie"];
+
+    // Get all
+    const status = await request(app).get("/api/status").set("Cookie", cookie);
+    expect(status.statusCode).toEqual(200);
+    expect(status.body).toEqual({
+      message: statusResponse,
+    });
+
+    // Get all
+    const dechets = await request(app)
+      .get("/api/dechets")
+      .set("Cookie", cookie);
+    expect(dechets.statusCode).toEqual(200);
+    expect(dechets.body).toEqual({
+      message: dechetResponse,
+    });
+
+    // Get all
+    const fonctions = await request(app)
+      .get("/api/fonction")
+      .set("Cookie", cookie);
+    expect(fonctions.statusCode).toEqual(200);
+    expect(fonctions.body).toEqual({
+      message: fonctionResponse,
+    });
+  });
+
+  test("Employe", async () => {
+    const list = await request(app).post("/api/login").send(Employe);
+    const cookie = list.headers["set-cookie"];
+
+    // Get all
+    const status = await request(app).get("/api/status").set("Cookie", cookie);
+    expect(status.statusCode).toEqual(200);
+    expect(status.body).toEqual({
+      message: statusResponse,
+    });
+
+    // Get all
+    const dechets = await request(app)
+      .get("/api/dechets")
+      .set("Cookie", cookie);
+    expect(dechets.statusCode).toEqual(200);
+    expect(dechets.body).toEqual({
+      message: dechetResponse,
+    });
+
+    // Get all
+    const fonctions = await request(app)
+      .get("/api/fonction")
+      .set("Cookie", cookie);
+    expect(fonctions.statusCode).toEqual(200);
+    expect(fonctions.body).toEqual({
+      message: fonctionResponse,
+    });
+  });
+
+  test("Chauffeur", async () => {
+    const list = await request(app).post("/api/login").send(Chauffeur);
+    const cookie = list.headers["set-cookie"];
+
+    // Get all
+    const status = await request(app).get("/api/status").set("Cookie", cookie);
+    expect(status.statusCode).toEqual(200);
+    expect(status.body).toEqual({
+      message: statusResponse,
+    });
+
+    // Get all
+    const dechets = await request(app)
+      .get("/api/dechets")
+      .set("Cookie", cookie);
+    expect(dechets.statusCode).toEqual(200);
+    expect(dechets.body).toEqual({
+      message: dechetResponse,
+    });
+
+    // Get all
+    const fonctions = await request(app)
+      .get("/api/fonction")
+      .set("Cookie", cookie);
+    expect(fonctions.statusCode).toEqual(200);
+    expect(fonctions.body).toEqual({
+      message: fonctionResponse,
+    });
+  });
+});
