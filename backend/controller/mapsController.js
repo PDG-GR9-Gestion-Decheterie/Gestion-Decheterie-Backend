e96f4d681c7ae155e35a62788fa269a83cc8f3a7
JustinFerrara14 <justin.ferrara@heig-vd.ch>
2024-08-30 08:57:40 +0200

    change test and modify api key

--- backend/controller/mapsController.js
+++ backend/controller/mapsController.js
@@ -1,6 +1,7 @@
 export async function getAPIKey(req, res) {
+  const APIKey = process.env.GOOGLE_MAPS_API_KEY;
   try {
-    res.status(200).json({ APIKey: "AIzaSyAcp4rBpazZ3oH9sR_eucOsfjjcyl5JKLo" });
+    res.status(200).json({ APIKey: APIKey });
   } catch (err) {
     console.error("Error generating map:", err);
     res.status(500).json({ error: "Error" });
